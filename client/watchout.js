ffbc30eaa92cca8250319fba47f5c3f458d8bfd0
aliotta <aliotta343@gmail.com>
2015-08-28 21:29:54 -0700

    Added a bunch of stuff power please dont go out!

--- client/watchout.js
+++ client/watchout.js
@@ -19,47 +19,60 @@ var gameboard = d3.select("body")
   .style("background-color", "lightgray");
 
 // Asteroid placement function
-var placeAsteroids = function(num){
+var placePolarBears = function(num){
   var output = [];
   var xCord;
   var yCord;
-  var radius;
+  var heightWidth;
   for (var i = 0; i < num; i++) {
     xCord = Math.random()*900;
     yCord = Math.random()*900;
-    radius = Math.random()*(30 - 10 +1)+10;
-    output.push({"xCord":xCord, "yCord" : yCord, "radius":radius});
+    heightWidth = Math.random()*(30 - 10 +1)+10;
+    output.push({"xCord":xCord, "yCord" : yCord, "heightWidth":heightWidth});
   };
   return output;
 }
 // Initialize asteroids on board
-var asteroids = placeAsteroids(15);
+var polarBears = placePolarBears(15);
 
 // Append asteroids to the DOM
-var asteroid = d3.select("svg")
-  .selectAll("circle")
-  .data(asteroids)
+d3.select("svg")
+  .selectAll("imgage")
+  .data(polarBears)
   .enter()
-  .append("circle")
-  .classed("asteroid", true)
-  .attr("cx", function (d) {return d.xCord})
-  .attr("cy", function (d) {return d.yCord})
-  .attr("r", function (d) {return d.radius})
-  .attr("stroke", "maroon")
-  .attr("stroke-width", "4")
-  .attr("fill", "grey");
+  .append("svg:image")
+  .classed("pbear", true)
+  .attr("x", function (d) {return d.xCord})
+  .attr("y", function (d) {return d.yCord})
+  .attr("height", function (d) {return d.heightWidth})
+  .attr("width", function (d) {return d.heightWidth})
+  .attr("collision", "false")
+  .attr("xlink:href", "PBear.png");
 
 // Add player to the DOM
 var player = d3.select("svg")
-  .append("circle")
+  .append("svg:image")
   .classed("player", true)
-  .attr("cx", "450")
-  .attr("cy", "450")
+  .attr("x", "450")
+  .attr("y", "450")
   .attr("r", "20")
-  .attr("stroke", "pink")
-  .attr("stroke-width", "4")
-  .attr("fill", "purple")
-  .attr("collision", "false");
+  .attr("collision", "false")
+  .attr("height", "60")
+  .attr("width", "60")
+  .attr("xlink:href", "penguin.png")
+  // .attr("background-imgage", "xlink:href", "http://rlv.zcache.com/penguin_face_birthday_party_invitation-r8338e73e5dae4a2abad360fee79f2f49_zk91c_512.jpg?rlvnet=1")
+
+
+
+// var player = d3.select("svg")
+
+
+// <svg width="640" height="480" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+//  <g>
+//   <title>Layer 1</title>
+//   <image xlink:href="http://rlv.zcache.com/penguin_face_birthday_party_invitation-r8338e73e5dae4a2abad360fee79f2f49_zk91c_512.jpg?rlvnet=1" id="svg_1" height="438" width="455" y="29" x="82"/>
+//  </g>
+// </svg>
 
 // Function to direct movement of player
 var playerCycle = function (coords) {
@@ -67,8 +80,8 @@ var playerCycle = function (coords) {
   var coords = d3.mouse(svg);
   d3.select("svg")
   .selectAll(".player")
-  .attr("cx", function (d){return coords[0]})
-  .attr("cy", function (d){return coords[1]});
+  .attr("x", function (d){return coords[0]-30})
+  .attr("y", function (d){return coords[1]-30});
   // distanceFromAsteroids();
   setTimeout(this.playerCycle.bind(this), 350);
 }
@@ -78,67 +91,83 @@ d3.select("svg").on("mousemove", function (){
 });
 
 // Generate new asteroid trajectory
-placeAsteroids.transition = function () {
+placePolarBears.transition = function () {
   vertMove = Math.random() * 900 + 1;
   horizMove = Math.random() * 900 + 1;
   return [''+horizMove, ''+vertMove];
 }
 
 // Timeout for sending asteroids to new locations
-placeAsteroids.asteroidCycle = function () {
+placePolarBears.polarBearCycle = function () {
   d3.select("svg")
-  .selectAll(".asteroid")
+  .selectAll(".pbear")
   .transition()
-  .attr("cx", function (d){return placeAsteroids.transition()[0]})
-  .attr("cy", function (d){return placeAsteroids.transition()[1]})
+  .attr("x", function (d){return placePolarBears.transition()[0]})
+  .attr("y", function (d){return placePolarBears.transition()[1]})
   .duration(1500);
 
-  setTimeout(this.asteroidCycle.bind(this), 1500);
+  setTimeout(this.polarBearCycle.bind(this), 1500);
 }
 
 // Initialize asteroid movement
-placeAsteroids.asteroidCycle();
+placePolarBears.polarBearCycle();
 
-// Collision instance counter
-var counter = 0;
+// Define counters
+var collisionCount = 0;
+var scoreCount = 0;
+var millisecondCount = 0;
+var highScore = 0;
 
 // Collision event handler
-var distanceFromAsteroids = function(){
-  var asteroidX;
-  var asteroidY;
-  var asteroidR;
-  var asteroids = d3.selectAll('.asteroid')[0];
-  var playerX = d3.select('.player')[0][0].cx.baseVal.value;
-  var playerY = d3.select('.player')[0][0].cy.baseVal.value;
-  var playerR = d3.select('.player')[0][0].r.baseVal.value;
+var distanceFromBears = function(){
+  // defining variables to use when calculating distance
+  var polarBearX;
+  var polarBearY;
+  var polarBearR;
+  var polarBears = d3.selectAll('.pbear')[0];
+  var playerX = d3.select('.player')[0][0].x.baseVal.value;
+  var playerY = d3.select('.player')[0][0].y.baseVal.value;
+
+  // update scoreCount each second
+  if (millisecondCount > 20) {
+    millisecondCount = 0;
+    scoreCount++;
+    // updating high score
+    if (scoreCount > highScore) {
+      highScore = scoreCount;
+    }
+  }
+  // iterate millisecondCount
+  millisecondCount++;
 
 
-
-
-  // Function using pythag to measure distance between player and each asteroid object
+  // Function using pythag to measure distance between player and each polarBear object
   var distance = function(x1, x2, y1, y2){
     return Math.sqrt((Math.abs(x2-x1) * Math.abs(x2-x1)) + (Math.abs(y2-y1) * Math.abs(y2-y1)));
   }
 
-
-  for (var i = 0; i<asteroids.length; i++){ 
-    asteroidX = asteroids[i].cx.baseVal.value;
-    asteroidY = asteroids[i].cy.baseVal.value;
-    asteroidR = asteroids[i].r.baseVal.value;
-
-    if(asteroids[i].collision === true && distance(playerX, asteroidX, playerY,asteroidY) > asteroidR + playerR){
-      counter++;
-      asteroids[i].collision = false;
+  for (var i = 0; i<polarBears.length; i++){ 
+  // saving polarBear coordinate and size values
+    polarBearX = polarBears[i].x.baseVal.value;
+    polarBearY = polarBears[i].y.baseVal.value;
+    // iterating collision count if player and polarBear have moved apart, adjusting scoreboard
+    
+    if(polarBears[i].collision === true && distance(playerX, polarBearX, playerY,polarBearY) > 40){//playerR){
+      collisionCount++;
+      polarBears[i].collision = false;
+      
     }
-
-    if (distance(playerX, asteroidX, playerY,asteroidY) < asteroidR + playerR) {
-      asteroids[i].collision = true;
-      scoreboard.selectAll('span')
-      .data([0,0, counter])
-      .text(function(d){return d})
+    // updating collision and score data
+    if (distance(playerX, polarBearX, playerY,polarBearY) < 40) {
+      polarBears[i].collision = true;
+      scoreCount = 0;
     }
   }
+  // updating the scoreboard
+  scoreboard.selectAll('span')
+  .data([highScore,scoreCount, collisionCount])
+  .text(function(d){return d})
 }   
 
-//Set the collision detection
-setInterval(distanceFromAsteroids, 50);  
+// Set the collision detection
+setInterval(distanceFromBears, 50);
